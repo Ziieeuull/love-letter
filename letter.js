f35e96af21ddbfbb85d0adab3f53f08699a9195a
Ziieeuull <128677038+Ziieeuull@users.noreply.github.com>
2023-11-21 20:30:31 +0800

    Add files via upload

---
+++ letter.js
@@ -0,0 +1,10 @@
+document.getElementById('heart').addEventListener('click', function () {
+    // Replace 'nextPage.html' with the desired URL
+    window.location.href = 'https://forms.gle/B1SAyqmCcfAZmXhi6';
+});
+
+document.getElementById('heart').addEventListener('click', function () {
+    var paper = document.getElementById('paper');
+    paper.contentEditable = true;
+    paper.focus();
+});
